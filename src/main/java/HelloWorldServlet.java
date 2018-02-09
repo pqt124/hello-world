028c133f7ffa85cc2a20f4b43c150d035cf7687d
pqt124 <1575712609@qq.com>
2018-02-10 01:27:19 +0800

    test

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class HelloWorldServlet extends javax.servlet.http.HttpServlet {
     public static void main(String args[]){
-        System.out.println("Hi World");
+        System.out.println("Hi World ee");
     }
 }
