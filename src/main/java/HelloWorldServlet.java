cbc91e9f3cd9acc9a894580d089a033a263368f3
pqt124 <1575712609@qq.com>
2018-02-10 01:26:59 +0800

    test

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,11 +1,7 @@
 import java.io.IOException;
 
 public class HelloWorldServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
-    }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    public static void main(String args[]){
+        System.out.println("Hi World");
     }
 }
